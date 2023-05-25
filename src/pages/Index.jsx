9b317d128669145fdc213b6462b014d9c61611f8
jankendall <jk.kuz20@gmail.com>
2023-05-25 16:46:47 +0800

    removed dom

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./TodoApp";
+import TodoApp from "./TodoApp";
 import TodoList from "./TodoList";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 
 function Index() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todolist" element={<TodoList />} />
-        <Route path="/todoitem" element={<TodoItem />} />
-        <Route path="/todoform" element={<TodoForm />} />
-      </Routes>
-    </Router>
+    <>
+      <TodoApp />
+      <TodoList />
+      <TodoItem />
+      <TodoForm />
+    </>
   );
 }
 
